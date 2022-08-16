e2063039453109bbe80632922949c4d73261eb11
Gabriel Sousa <104873711+GS-Gabriel@users.noreply.github.com>
2022-08-16 14:19:23 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 // ----------------- Atribuição ----------------------
 var player = "sage"
 var playerX = player
-var playerO = "cypher"
+var playerO = "astra"
 var numJog = 0
 var finalizacao  = false
 const Player1 = localStorage.getItem('player1')
@@ -83,7 +83,7 @@ function checkjogo(id) {
             player = playerX
         }
     }
-    if (cpu == "true" && player == "cypher") {
+    if (cpu == "true" && player == "astra") {
         setTimeout(() => {checkjogo(jogoDoPc())}, 500)
     }
     function jogoDoPc() {
